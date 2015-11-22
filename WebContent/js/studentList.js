bf8782fd6bcc7b1dfbd6eef4d09552051a7bf6b9
蒋文杨 <u19950930@gmail.com>
2015-11-22 20:37:34 +0800

    获取学生列表

--- WebContent/js/studentList.js
+++ WebContent/js/studentList.js
@@ -1,3 +1,15 @@
 /**
  * Created by dust on 2015/11/22.
  */
+$(function(){
+    getStudentList();
+});
+function getStudentList(){
+    $.ajax({
+        type:'POST',
+        url:'',
+        success: function (data, statusText) {
+            
+        }
+    });
+}
