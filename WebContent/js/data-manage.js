b1544a25a1f2b692ee69b3ffc7d03fefbfc0b49f
蒋文杨 <u19950930@gmail.com>
2015-12-15 14:47:01 +0800

    文件管理完成

--- WebContent/js/data-manage.js
+++ WebContent/js/data-manage.js
@@ -4,20 +4,31 @@
 $(function () {
     getFileList();
     uploadFile();
+    download();
 });
 //获取文件列表
+/*参数：无
+* 返回：json字符串
+* */
 function getFileList() {
+    var $fileList=$('.file-list ul');
     $.ajax({
         type: "GET",
         url: "../source?operate=getFileList",
         success: function (data) {
-            var jsondata= $.parseJSON(data);
-            console.log(jsondata);
+            var jsondata= $.parseJSON(data).sourseList;
             var html = "";
+            $.each(jsondata, function (index,value) {
+                html+=" <li class='list-group-item'><span class='glyphicon glyphicon-file'></span><span class='none fileId'>"+jsondata[index].sourceID+"</span><span class='headline'>"+jsondata[index].headline+"</span><a href='javascript:void(0);' class='download'><span class='glyphicon glyphicon-download'></span></a></li>";
+            })
+            $fileList.empty().append(html);
         }
     });
 }
 //点击上传按钮上传文件
+/*参数：文件名和数据
+* 返回:1,0
+*/
 function uploadFile() {
     var $upload = $('.upload');
     var $fileForm = $upload.find('form');
@@ -31,9 +42,26 @@ function uploadFile() {
                     $(this).resetForm();
                     alert('共享成功！');
                     getFileList();
-                } else {
-                    alert(statusText);
+                } else if(data==0) {
+                    alert("上传失败，请重试！");
                 }
+            }
+        });
+    })
+}
+//点击下载按钮下载文件
+/*参数：fileID
+* 返回：后台调用浏览器进行下载
+*/
+function download(){
+    var $fileList=$('.file-list ul');
+    $fileList.on('click','.download',function () {
+        var thisID=$(this).prev().prev().html();
+        $.ajax({
+            type:"POST",
+            url:"../source?operate=download",
+            data:{
+                sourceID:thisID,
             }
         });
     })
