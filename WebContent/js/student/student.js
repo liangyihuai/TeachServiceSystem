97e55e91530bf6530a92faddeb60d169fcf916f9
jiangwenyang <u19950930@gmail.com>
2016-01-01 17:12:25 +0800

    bug

--- WebContent/js/student/student.js
+++ WebContent/js/student/student.js
@@ -15,11 +15,27 @@ function init(){
     $('#current_user').text($.cookie('current_student'));
     $('#current_course a').text(decodeURI($.cookie('courseName')));
 }
-
+//跨浏览器获取视口大小
+function getInner(){
+    if(typeof window.innerWidth!='undefined'){
+        return{
+            width:window.innerWidth,
+            height:window.innerHeight
+        }
+    }else{
+        return{
+            width:document.documentElement.clientWidth,
+            height:document.documentElement.clientHeight
+        }
+    }
+}
 //工具按钮、控制环
 function setting(){
     $('#info').click(function () {
-        $('#curent_center').css({'width': $(document).width(), 'height': $(document).height()}).fadeIn();
+        $('#curent_center').css({'width': getInner().width, 'height': getInner().height}).fadeIn();
+        window.resize=function(){
+            $('#curent_center').css({'width': getInner().width, 'height': getInner().height}).fadeIn();
+        }
         $(this).hide();
     })
     $('.close').click(function () {
